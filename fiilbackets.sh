1df3ff51dc8f61899a9ddcd5e266b3b3ef6ce4de
ruslan.usifov@gmail.com <ruslan.usifov@gmail.com>
2012-02-01 00:03:07 +0400

    cleanup

--- fiilbackets.sh
+++
@@ -1,5 +0,0 @@
-./py-nsupdate -h localhost:9160 -f add_domain g21.g300.net 120
-./py-nsupdate -h localhost:9160 -f add_domain_backet g21.g300.net EUR 10.68 40 True
-./py-nsupdate -h localhost:9160 -f add_domain_backet g21.g300.net USA -73.68 40 120 False
-./py-nsupdate -h localhost:9160 -f add_domain_backet_ip g21.g300.net USA 174.36.214.36
-./py-nsupdate -h localhost:9160 -f rmv_domain_backet_ip g21.g300.net USA 174.36.214.36
