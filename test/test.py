c3304c42854dcd86bebbfc0b76ee86800eb7f2e1
ruslan.usifov@gmail.com <ruslan.usifov@gmail.com>
2012-01-31 12:25:13 +0400

    reorganize repo struct
    --HG--
    rename : pythonbackend/BorrowRefCount.h => src/BorrowRefCount.h
    rename : pythonbackend/Makefile.am => src/Makefile.am
    rename : pythonbackend/NewRefCount.h => src/NewRefCount.h
    rename : pythonbackend/OBJECTFILES => src/OBJECTFILES
    rename : pythonbackend/OBJECTLIBS => src/OBJECTLIBS
    rename : pythonbackend/PyDNSPacket.cc => src/PyDNSPacket.cc
    rename : pythonbackend/PyDNSPacket.h => src/PyDNSPacket.h
    rename : pythonbackend/PyDNSResourceRecord.cc => src/PyDNSResourceRecord.cc
    rename : pythonbackend/PyDNSResourceRecord.h => src/PyDNSResourceRecord.h
    rename : pythonbackend/PyDomainInfo.cc => src/PyDomainInfo.cc
    rename : pythonbackend/PyDomainInfo.h => src/PyDomainInfo.h
    rename : pythonbackend/PyGILLock.cc => src/PyGILLock.cc
    rename : pythonbackend/PyGILLock.h => src/PyGILLock.h
    rename : pythonbackend/PyQType.cc => src/PyQType.cc
    rename : pythonbackend/PyQType.h => src/PyQType.h
    rename : pythonbackend/PySOAData.cc => src/PySOAData.cc
    rename : pythonbackend/PySOAData.h => src/PySOAData.h
    rename : pythonbackend/RefCount.h => src/RefCount.h
    rename : pythonbackend/pythonbackend.cc => src/pythonbackend.cc
    rename : pythonbackend/pythonbackend.hh => src/pythonbackend.hh

--- test/test.py
+++ test/test.py
@@ -1,10 +1,33 @@
-import mmap;
-import time;
+#!/usr/bin/python
 
-with open("hello.txt", "r+") as f:
-  while True:
-    map = mmap.mmap(f.fileno(), 1000);
-    print map[0:9];
+import GeoIP;
+import math;
 
-    time.sleep(1);
+def distance(point1, point2):
+  retval = -1;
 
+  try:
+    l_longitude = math.pow(point1['longitude'] - point2['longitude'], 2);
+    l_latitude = math.pow(point1['latitude'] - point2['latitude'], 2);
+
+    retval = math.sqrt(l_longitude + l_latitude);
+
+  except:
+    pass;
+
+  return retval;
+
+
+gi = GeoIP.open("/usr/share/GeoIP/GeoIPCity.dat", GeoIP.GEOIP_MEMORY_CACHE);
+#gi = GeoIP.open("/usr/share/GeoIP/GeoIP.dat", GeoIP.GEOIP_MEMORY_CACHE);
+
+ips = ('188.27.155.221', '188.134.33.152', '108.59.160.189', '92.54.194.79', '178.45.19.175', '81.163.176.44', '85.141.100.115', '164.127.240.24', '69.171.228.249', '66.220.158.251', '94.232.177.12', '75.130.92.205');
+amsterdamcoords={'longitude':4.89, 'latitude': 52.37};
+
+for ip in ips:
+    l_ip1record = gi.record_by_addr(ip);
+    l_distance = distance(amsterdamcoords, l_ip1record);
+
+    print "for ip: %s" % (ip);
+    print l_distance
+    print "", ""
